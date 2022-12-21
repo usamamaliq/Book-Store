b0514e77e30b700ccebc3647b345447e3f42f0d8
Usama Masood <usamamaliq@gmail.com>
2022-12-22 02:31:46 +0500

    Fix linters

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,18 +1,16 @@
-
 const ADD = 'books/books/ADD';
 const REMOVE = 'books/books/REMOVE';
 
-
 export default function reducer(state = [], action = {}) {
-    switch (action.type) {
-      case ADD:
-        return state
-      case REMOVE:
-        return state
-      default:
-        return state;
-    }
+  switch (action.type) {
+    case ADD:
+      return state;
+    case REMOVE:
+      return state;
+    default:
+      return state;
   }
+}
 export const createBook = (book) => ({
   type: ADD,
   text: book,
