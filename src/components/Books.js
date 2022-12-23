4c173f6ae73182a74094f69fe348bdcd69900713
Usama Masood <usamamaliq@gmail.com>
2022-12-23 11:54:26 +0500

    Display books using useSelector

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,12 +1,24 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import BookInput from './BookInput';
 
-const Books = () => (
-  <div className="BooksContainer">
-    <Book title="Awaken the Giant Within" author="Anthony Robins" />
-    <BookInput />
-  </div>
-);
+const Books = () => {
+  const books = useSelector((state) => state.books);
+  return (
+    <>
+      <div className="BooksContainer">
+        {books.map((book) => (
+          <Book
+            key={book.id}
+            title={book.title}
+            author={book.author}
+            id={book.id}
+          />
+        ))}
+        <BookInput />
+      </div>
+    </>
+  );
+};
 export default Books;
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,20 @@
 const ADD = 'books/books/ADD';
 const REMOVE = 'books/books/REMOVE';
 
-export default function reducer(state = [], action = {}) {
+const initialState = [
+  {
+    title: 'Awaken the Giant Within',
+    author: 'Anthony Robins',
+    id: 1,
+  },
+  {
+    title: 'Harry Potter',
+    author: 'J.K Rowlings',
+    id: 2,
+  },
+];
+
+export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case ADD:
       return state;
