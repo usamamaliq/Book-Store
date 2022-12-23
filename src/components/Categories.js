a7714655d91858e14b96ec9e7132b3bda84a28de
Usama Masood <usamamaliq@gmail.com>
2022-12-23 13:25:47 +0500

    Add functionality for categories button through action

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,8 +1,28 @@
-const Categories = () => (
-  <div>
-    <button className="statusButton" type="button">Check Status</button>
-  </div>
+import { useSelector, useDispatch } from 'react-redux';
+import { Status } from '../redux/categories/categories';
 
-);
+const Categories = () => {
+  const categories = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>
+        {categories}
+      </h1>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          dispatch(Status());
+        }}
+        className="statusButton"
+        type="button"
+      >
+        Check Status
+      </button>
+    </div>
+
+  );
+};
 
 export default Categories;
